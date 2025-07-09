8806e707bd836a39df3f9087d275ab4e4a949d3c
Arnaldo tomo <arnaldotomo@gmail.com>
2025-07-09 23:04:08 +0200

    Remove analytics and settings routes
    Deleted routes related to analytics and settings from web.php, including their associated controllers. This streamlines the routing file by removing unused or deprecated features.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
-
-// routes/web.php
-
 use App\Http\Controllers\AlertController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\AnalyticsController;
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -28,17 +19,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/alerts/{alert}', [AlertController::class, 'show'])->name('alerts.show');
     Route::delete('/alerts/{alert}', [AlertController::class, 'destroy'])->name('alerts.destroy');
 
-    // Analytics e Relatórios
-    Route::get('/analytics', [AnalyticsController::class, 'index'])->name('analytics.index');
-    Route::get('/analytics/export', [AnalyticsController::class, 'export'])->name('analytics.export');
-    Route::get('/analytics/data', [AnalyticsController::class, 'getData'])->name('analytics.data');
+
 
     // Mapa de Alertas
     Route::get('/map', [AlertController::class, 'mapView'])->name('alerts.map');
 
-    // Configurações
-    Route::get('/settings', [DashboardController::class, 'settings'])->name('settings.index');
-    Route::post('/settings', [DashboardController::class, 'updateSettings'])->name('settings.update');
+
 
     // Perfil do Usuário
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
